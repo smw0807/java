a0ccda296fe628c987351f9f11e311f65e0b583a
smw0807 <smw0807@gmail.com>
2026-07-08 20:45:36 +0900

    변수 타입1

---
+++ java-start/src/variable/Var7.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 10; // 정수 리터럴
+        double b = 10.5; // 실수 리터럴
+        boolean c = true; // 불리언 리터럴
+        char d = 'A'; // 문자 하나 리터럴
+        String e = "Hello Java"; // 문자열 리터럴
+        // 리터럴 : 사용자가 직접 입력한 고정된 값
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
