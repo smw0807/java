205e9bb10ed226aa1919312cfb91e32008cc6104
smw0807 <smw0807@gmail.com>
2026-07-09 21:26:52 +0900

    변수 타입2

---
+++ java-start/src/variable/Var8.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte a = 127;
+        short b = 32767;
+        int c = 2147483647;
+
+        long d = 9223372036854775807L;
+
+        // 실수
+        float e = 3.14f;
+        double g = 1.7976931348623157E308;
+
+        
+    }
+}
