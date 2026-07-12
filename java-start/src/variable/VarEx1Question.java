dd481a6326999e9e70fdd386175d8fbc5ab7eac1
smw0807 <smw0807@gmail.com>
2026-07-12 12:21:48 +0900

    문제

---
+++ java-start/src/variable/VarEx1Question.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var1Ex1Question {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+
+        num1 = 4;
+        num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ java-start/src/variable/VarEx3.java
@@ -0,0 +1,8 @@
+package variable;
+public class VarEx3 {
+  public static void main(String[] args) {
+    long longVar = 10000000000L;
+
+    System.out.println("longVar: " + longVar);
+  }
+}
