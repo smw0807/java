6b9938b53f1aed0098b09b9d68043ee11b6a11cb
smw0807 <smw0807@gmail.com>
2026-07-12 20:26:04 +0900

    문제

---
+++ java-start/src/operator/OperationEx2.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        double a = 1.5;
+        double b = 2.5;
+        double c = 3.5;
+
+        double sum = a + b + c;
+        double avg = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(avg);
+    }
+}
