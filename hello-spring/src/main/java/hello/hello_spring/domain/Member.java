69706632d2a4680a80d62f7ebef5bd273cd555a3
smw0807 <smw0807@gmail.com>
2026-07-25 21:21:43 +0900

    회원 도메인과 리포지토리 만들기

---
+++ hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -0,0 +1,19 @@
+package hello.hello_spring.domain;
+
+public class Member {
+    private long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
