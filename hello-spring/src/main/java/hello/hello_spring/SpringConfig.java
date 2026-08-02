b606fe7fdb2520d531502e2ffcd4ed7a79947a57
smw0807 <smw0807@gmail.com>
2026-08-02 11:30:18 +0900

    스프링 데이터 JPA

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -12,23 +12,24 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager  entityManager;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new JdbcMemberRepository(dataSource);
 //        return new MemoryMemberRepository();
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(entityManager);
-    }
+//        return new JpaMemberRepository(entityManager);
+//    }
 }
