08186a94c8ca29417d8090f8627625834be4ce79
smw0807 <smw0807@gmail.com>
2026-08-05 21:42:43 +0900

    기본형과 참조형

---
+++ java-basic/src/ref/VarChange2.java
@@ -0,0 +1,25 @@
+package ref;
+
+public class VarChange2 {
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        Data dataB = dataA;
+
+        System.out.println("dataA Ref = " + dataA);
+        System.out.println("dataB Ref = " + dataB);
+
+        System.out.println("dataA.value = " + dataA.value);
+        System.out.println("dataB.value = " + dataB.value);
+
+        dataA.value = 20;
+        System.out.println("change dataA.value = 20");
+        System.out.println("dataA value = " + dataA.value);
+        System.out.println("dataB.value = " + dataB.value);
+
+        dataB.value = 30;
+        System.out.println("change dataB.value = 30");
+        System.out.println("dataA value = " + dataA.value);
+        System.out.println("dataB.value = " + dataB.value);
+    }
+}
