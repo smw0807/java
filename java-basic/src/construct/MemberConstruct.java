0b4cda68876fe96e685673de8cf65dc08edd4632
smw0807 <smw0807@gmail.com>
2026-08-15 13:59:25 +0900

    생성자 오버로딩과 this()

--- java-basic/src/construct/MemberConstruct.java
+++ java-basic/src/construct/MemberConstruct.java
@@ -5,6 +5,11 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name, int age) {
+        // this() 는 첫 줄에 작성해야지만 정상작동됨
+        this(name, age, 50);
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + " age=" + age + " grade=" + grade);
         this.name = name;
