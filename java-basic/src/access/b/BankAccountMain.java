ba41d70c6d9928faff5d94388458637394573522
smw0807 <smw0807@gmail.com>
2026-08-16 17:58:38 +0900

    문제와 풀이

---
+++ java-basic/src/access/b/BankAccountMain.java
@@ -0,0 +1,18 @@
+package access.b;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(10000);
+        bankAccount.withdraw(3000);
+        System.out.println("balance="+bankAccount.getBalance());
+
+        bankAccount.withdraw(3000);
+        System.out.println("balance="+bankAccount.getBalance());
+        bankAccount.withdraw(3000);
+        System.out.println("balance="+bankAccount.getBalance());
+        bankAccount.withdraw(3000);
+        System.out.println("balance="+bankAccount.getBalance());
+
+    }
+}
