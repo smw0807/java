597f414a0faa63561af97fbad53be463340bfde9
smw0807 <smw0807@gmail.com>
2026-08-17 17:23:50 +0900

    static이 필요한 이유

---
+++ java-basic/src/static1/DataCounterMain2.java
@@ -0,0 +1,15 @@
+package static1;
+
+public class DataCounterMain2 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        Data2 data1 = new Data2("A", counter);
+        System.out.println("A counter=" + counter.count);
+
+        Data2 data2 = new Data2("B", counter);
+        System.out.println("B counter=" + counter.count);
+
+        Data2 data3 = new Data2("C", counter);
+        System.out.println("C counter=" + counter.count);
+    }
+}
