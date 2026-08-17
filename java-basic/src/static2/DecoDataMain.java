e7b7b8d0cfca51ff62b39a5239fcbf5389a1fe2d
smw0807 <smw0807@gmail.com>
2026-08-17 20:58:03 +0900

    static import

--- java-basic/src/static2/DecoDataMain.java
+++ java-basic/src/static2/DecoDataMain.java
@@ -1,9 +1,15 @@
 package static2;
 
+import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
