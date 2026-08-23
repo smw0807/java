daa8976afa63434e2392f584ac1ecfb4964dc35a
smw0807 <smw0807@gmail.com>
2026-08-23 12:55:55 +0900

    super 생성자

---
+++ java-basic/src/extends1/super2/Super2Main.java
@@ -0,0 +1,9 @@
+package extends1.super2;
+
+public class Super2Main {
+    public static void main(String[] args) {
+        ClassC c = new ClassC();
+        System.out.println("----------");
+        ClassB b = new ClassB(100);
+    }
+}
