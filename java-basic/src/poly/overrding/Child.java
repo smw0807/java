1b1ce5db4fc254ef5dccb89fb6b86af97e40d3dd
smw0807 <smw0807@gmail.com>
2026-08-27 22:32:47 +0900

    다형성과 메서드 오버라이딩

---
+++ java-basic/src/poly/overrding/Child.java
@@ -0,0 +1,10 @@
+package poly.overrding;
+
+public class Child extends Parent {
+    public String value = "child";
+
+    @Override
+    public void method() {
+        System.out.println("Child.method");
+    }
+}
